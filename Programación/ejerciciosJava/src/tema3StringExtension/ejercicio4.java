fb45bc77e5cba7db939c6eeb3f92626b69efab59
RaulSal <salromeoraul@gmail.com>
2025-12-10 12:18:46 +0100

    ejercicio4

---
+++ Programación/ejerciciosJava/src/tema3StringExtension/ejercicio4.java
@@ -0,0 +1,8 @@
+package tema3StringExtension;
+import java.util*;
+public class ejercicio4{
+    private static Scanner teclado = new Scanner (System.in);
+    public static void main (String[]args){
+
+    }s
+}
