20e24c7162ec397be3328dc3e423adbc8075755b
RaulSal <salromeoraul@gmail.com>
2025-12-10 12:25:37 +0100

    ejercicios

---
+++ Programación/ejerciciosJava/src/tema3StringExtension/ejercicio5.java
@@ -0,0 +1,14 @@
+/*Realizar un programa que cree el diminutivo de un nombre que se solicitará por teclado.
+Si el nombre termina en O, se sustituye esta letra final por el sufijo “ITO” .
+Si termina en “A” , IDEM con el sufijo “ITA”.
+Si el nombre termina en E,I, o U se preguntará si es masculino o femenino y se procederá de la misma
+forma.
+Si el nombre termina en consonante se preguntará si es masculino o femenino y se procederá
+añadiendo ITO o ITA.
+Si no cumple ninguna de las anteriores condiciones, se mostrará por pantalla con “Nombre
+incorrecto”
+NOTAS:
+• El diminutivo se devolverá siempre en mayúsculas.
+• Si el dato del sexo es incorrecto se volverá a solicitar.
+• Debe usarse un método para crear el diminutivo.*/
+package tema3StringExtension;
