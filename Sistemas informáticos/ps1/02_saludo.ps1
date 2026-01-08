ef743133f6c0c4eda76ffb9869398bd31836b6ed
RaulSal <salromeoraul@gmail.com>
2026-01-08 16:11:14 +0100

    Delete Sistemas informáticos directory

--- Sistemas informáticos/ps1/02_saludo.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Como te llamas: "
-$nombre = Read-Host " "
-$nada = ""
-if($nombre -eq $nada) {
-    $nombre = "desconocido"
-    Write-Host "Hola $nombre"
-} else{
-Write-Host "Hola $nombre"
-}
