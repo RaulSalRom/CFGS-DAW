ce010b426c0dea9c19a614916bb4e03a9aee287c
RaulSalRom <salromeoraul@gmail.com>
2026-01-19 10:56:03 +0100

    ejercicio2

--- Programación/ejerciciosJava/tema4/ejercicio1/Rectangulo.java
+++ Programación/ejerciciosJava/tema4/ejercicio1/Rectangulo.java
@@ -1,16 +1,16 @@
-package Programación.ejerciciosJava.tema4.ejercicio1; // Sin tilde
+package Programación.ejerciciosJava.tema4.ejercicio1;
 
 public class Rectangulo {
-    private double longitud = 1; // Valor predeterminado [cite: 179]
+    private double longitud = 1;
     private double ancho = 1;
 
-    public Rectangulo() { // Constructor vacío para coincidir con el main [cite: 278]
-        // Ya inicializados arriba
+    public Rectangulo() { // Constructor vacío para coincidir con el main
+
     }
 
     public void setLongitud(double longitud) {
-        if (longitud > 0 && longitud < 20) { // Validación [cite: 256]
-            this.longitud = longitud; // 'this' resuelve la ambigüedad [cite: 348]
+        if (longitud > 0 && longitud < 20) {
+            this.longitud = longitud;
         }
     }
 
@@ -29,7 +29,7 @@ public class Rectangulo {
     }
 
     public double calcularPerimetro() {
-        return 2 * (longitud + ancho); // Fórmula corregida
+        return 2 * (longitud + ancho);
     }
 
     public double calcularArea() {
