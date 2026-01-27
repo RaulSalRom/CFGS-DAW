f6359b02dcf4d8f8df2eaac376d0a7ef21cee554
RaulSalRom <salromeoraul@gmail.com>
2026-01-27 19:22:15 +0100

    ejercicio máquina

--- tema4/ejercicio3/Maquina.java
+++ tema4/ejercicio3/Maquina.java
@@ -1,145 +1,115 @@
-
 public class Maquina {
+    private int depositoCafe;
+    private int depositoLeche;
+    private int depositoVasos;
+    private double monedero;
+    
+    
+    
+    public Maquina(double monederoInicial) {
+        this.depositoCafe = 50;
+        this.depositoLeche = 50;
+        this.depositoVasos = 80;
+        this.monedero = monederoInicial;
+    }
 
-	private int depositoCafe;
-	private int depositoLeche;
-	private int depositoVasos;
-	private double monedero;
-	private double saldo;
+    public void servirCafe(double pagoCliente) {
+        double precio = 1.0;
+     
+        if (depositoCafe < 1 || depositoVasos < 1) {
+            System.out.println("Error: No hay existencias de café o vasos.");
+        } 
+        
+        else if (pagoCliente < precio) {
+            System.out.println("Error: Dinero insuficiente.");
+        }
+        
+        else if (monedero < (pagoCliente - precio)) {
+            System.out.println("Error: La máquina no tiene cambio.");
+        } 
+        
+        else {
+            depositoCafe--;
+            depositoVasos--;
+            double cambio = pagoCliente - precio;
+            monedero += precio;
+            System.out.println("Producto servido. Recoge tu cambio de " + cambio + " euros.");
+        }
+    }
 
-	public Maquina() {
-		depositoCafe = 50;
-		depositoLeche = 50;
-		depositoVasos = 80;
-		monedero = 0;
-	}
-	
-	public void setDepositoCafe(int depositoCafe) {
-		this.depositoCafe = depositoCafe;
-	}
-	
-	public int getDepositoCafe() {
-		return this.depositoCafe;
-	}
-	
-	public void setDepositoLeche (int depositoLeche) {
-		this.depositoLeche = depositoLeche;
-	}
-	
-	public int getDepositoLeche() {
-		return this.depositoLeche;
-	}
-	
-	public void setDepositoVasos(int depositosVasos) {
-		this.depositoVasos = depositosVasos;
-	}
-	
-	public int getDepositoVasos() {
-		return this.depositoVasos;
-	}
-	
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-	public double getSaldo() {
-		return this.saldo;
-	}
-	
-	public void servirCafe() {
-		
-		System.out.println("Su café se está sirviendo...");
-		depositoCafe -= 1;
-		monedero -=1;
-		saldo ++;
-		System.out.println("Recoja su cambio: " + monedero);
-		
-	}
-	public void servirLeche() {
-		
-		System.out.println("Su leche se está sirviendo...");
-		depositoLeche -= 1;
-		monedero -= 0.8;
-		saldo += 0.8;
-		System.out.print("Recoja su cambio: " + monedero);
-		
-	}
-	
-	public void servirMezcla() {
-		
-		System.out.println("Su café con leche se está sirviendo...");
-		depositoLeche -= 1;
-		depositoCafe -= 1;
-		monedero -= 1.5;
-		saldo += 1.5;
-		System.out.println("Recoja su cambio: " + monedero);
-		
-	}
-	
-	public void consulta() {
-		System.out.println("Estamos en el apartado de consulta");
-		System.out.println("Servicios restantes de café: " + depositoCafe);
-		System.out.println("Servicios restantes de leche: " + depositoLeche);
-		System.out.println("Servicio restantes de vasos: " + depositoVasos);
-		System.out.println("Saldo de la máquina" + saldo);
-	}
-	
-	public void llenarDepositos() {
-		
-		if(depositoCafe < 50) {
-		
-			int x = 0;
-			System.out.println("Se está llenando su deposito de café");
-			while(depositoCafe < 50) {
-				
-				depositoCafe++;
-				x ++;
-				
-			}
-			System.out.println("Su deposito de café está lleno");
-			System.out.println("Se han añadido " + x + " servicios de café");
-			
-		}
-		
-		if(depositoLeche < 50) {
-			
-			int y = 0;
-			System.out.println("Se está llenando su deposito de leche");
-			while(depositoLeche < 50) {
-				
-				depositoLeche ++;
-				y ++;
-				
-			}
-			
-			System.out.println("Su deposito de leche está lleno");
-			System.out.println("Se han añadido " + y + " servicios más");
-			
-		}
-		if(depositoVasos < 80) {
-			
-			int z = 0;
-			System.out.println("Se están lavando y reponiendo los vasos");
-			while(depositoVasos < 80) {
-				
-				depositoVasos ++;
-				z ++;
-				
-			}
-			
-			System.out.println("Se han repuesto los vasos");
-			System.out.println("Se han repuesto " + z + " vasos");
-			
-		}
-	}
-		
-		
-	
-	public void sacarDinero() {
-		
-	}
-	
+    
+    
+    public void servirLeche(double pagoCliente) {
+        double precio = 0.8;
+        if (depositoLeche < 1 || depositoVasos < 1) {
+            System.out.println("Error: No hay existencias de leche o vasos.");
+        } 
+        
+        else if (pagoCliente < precio) {
+            System.out.println("Error: Dinero insuficiente.");
+        } 
+        
+        else if (monedero < (pagoCliente - precio)) {
+            System.out.println("Error: La máquina no tiene cambio.");
+        } 
+        
+        else {
+            depositoLeche--;
+            depositoVasos--;
+            double cambio = pagoCliente - precio;
+            monedero += precio;
+            System.out.println("Producto servido. Recoge tu cambio de " + cambio + " euros.");
+        }
+    }
+
+  
+    public void servirMezcla(double pagoCliente) {
+        double precio = 1.5;
+        if (depositoCafe < 1 || depositoLeche < 1 || depositoVasos < 1) {
+            System.out.println("Error: No hay existencias suficientes.");
+        }
+
+        else if (pagoCliente < precio) {
+            System.out.println("Error: Dinero insuficiente.");
+        } 
+        
+        else if (monedero < (pagoCliente - precio)) {
+            System.out.println("Error: La máquina no tiene cambio.");
+        } 
+        
+        else {
+            depositoCafe--;
+            depositoLeche--;
+            depositoVasos--;
+            double cambio = pagoCliente - precio;
+            monedero += precio;
+            System.out.println("Producto servido. Recoge tu cambio de " + cambio + " euros.");
+        }
+    }
+
+    public void consultarEstado() {
+    	
+        System.out.println("--- ESTADO DE LA MÁQUINA ---");
+        System.out.println("Café: " + depositoCafe + " dosis");
+        System.out.println("Leche: " + depositoLeche + " dosis");
+        System.out.println("Vasos: " + depositoVasos + " unidades");
+        System.out.println("Monedero: " + monedero + " euros");
+        
+    }
+
+    public void llenarDepositos() {
+    	
+        this.depositoCafe = 50;
+        this.depositoLeche = 50;
+        this.depositoVasos = 80;
+        System.out.println("Depósitos llenos.");
+        
+    }
+
+    public void vaciarMonedero() {
+    	
+        System.out.println("Se han retirado " + this.monedero + " euros.");
+        this.monedero = 0;
+        
+    }
 }
-
-
-
-
