caad9bc2469ca8c6718c1f207a5f181e3be064ad
RaulSalRom <salromeoraul@gmail.com>
2026-02-05 11:22:01 +0100

    12)

--- Sistemas Informaticos/2ndo trimestre/ejercicios/Ejercicios de bash básicos/archivos.sh
+++ Sistemas Informaticos/2ndo trimestre/ejercicios/Ejercicios de bash básicos/archivos.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+ruta=$1
+if [[ -e "$ruta" ]]
+	then
+    	echo "La ruta '$ruta' existe." [cite: 244]
+
+ if [[ -d "$ruta" ]]
+	then
+        echo "- Es un directorio."
+    elif [[ -f "$ruta" ]]
+	then
+        echo "- Es un archivo regular"
+    elif [[ -L "$ruta" ]]
+	then
+        echo "- Es un enlace simbólico." 
+    fi
+    if [[ -r "$ruta" ]]
+	 then
+        echo "- Es legible (lectura)." 
+    fi
+
+    if [[ -w "$ruta" ]]
+	 then
+        echo "- Es escribible (escritura)." 
+    fi
+
+    if [[ -x "$ruta" ]]
+	 then
+        echo "- Es ejecutable." 
+    fi
+
+else
+    echo "La ruta '$ruta' NO existe."
+fi
