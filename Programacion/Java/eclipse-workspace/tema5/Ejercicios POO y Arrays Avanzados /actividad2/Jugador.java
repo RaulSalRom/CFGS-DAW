664fed686d72d468db6b852e008c94f225137e4c
RaulSalRom <salromeoraul@gmail.com>
2026-03-11 22:55:44 +0100

    actualizacion

---
+++ Programacion/Java/eclipse-workspace/tema5/Ejercicios POO y Arrays Avanzados /actividad2/Jugador.java
@@ -0,0 +1,24 @@
+package actividad2;
+
+public class Jugador {
+
+	private String nombre;
+	
+	Jugador(){
+		
+		this.nombre = nombre;
+		
+	}
+	
+	public void setNombre() {
+		
+		this.nombre = nombre;
+		
+	}
+	
+	public String getNombre() {
+		
+		return this.nombre;
+		
+	}
+}
---
+++ Programacion/Java/eclipse-workspace/tema5/Ejercicios POO y Arrays Avanzados /actividad2/main.java
@@ -0,0 +1,18 @@
+package actividad2;
+
+import java.util.*;
+
+public class main {
+	
+	private static Scanner teclado = new Scanner(System.in);
+	
+	public static void main(String[]args) {
+		
+		Jugador jugador1 = new Jugador();
+		
+		Jugador jugador2 = new Jugador();
+		
+		
+		
+	}
+}
