009b32bad35a26e2bd5c68d163089b4f1ec2aaff
RaulSalRom <salromeoraul@gmail.com>
2026-03-17 23:17:49 +0100

    relacion 9 java

--- Programacion/Java/eclipse-workspace/tema5/Ejercicios POO y Arrays Avanzados /actividad3/FaltasAlumnosAsignaturas.java
+++ Programacion/Java/eclipse-workspace/tema5/Ejercicios POO y Arrays Avanzados /actividad3/FaltasAlumnosAsignaturas.java
@@ -1,153 +1,149 @@
 package actividad3;
 
 public class FaltasAlumnosAsignaturas {
-	
+    // Atributos privados para encapsular los datos
+   
 	private String[] asignaturas;
-	
+    
 	private Falta[][] alumnos;
-	
-	
-	FaltasAlumnosAsignaturas(int i, String[] asignaturas){
-		
+
+    // Constructor: Reserva el espacio en memoria para la matriz [filas][columnas]
+   
+	FaltasAlumnosAsignaturas(int i, String[] asignaturas) {
+      
 		this.asignaturas = asignaturas;
-		
-		
+        
 		this.alumnos = new Falta[i][asignaturas.length];
-		
+ 
 	}
-	
-	public void asignarMatriz(int i, String[] asignaturas) {
-		
-		for (int x = 0; x < i; x ++) {
-			
-			for (int y = 0; y < asignaturas.length; y++) {
-				
-				this.alumnos[x][y] = new Falta();
-				
-			}
-			
-		}
-		
-	}
-	
-	public void mayoresInjustificadas(int i, String[] asignaturas) {
-		
 
+    // Método para inicializar cada celda de la matriz con un objeto Falta y datos aleatorios
+   
+	public void inicializarYReflejarDatos() {
+       
+		for (int x = 0; x < alumnos.length; x++) { // Recorre cada fila (alumno)
+            
+			for (int y = 0; y < asignaturas.length; y++) { // Recorre cada columna (asignatura)
+               
+				this.alumnos[x][y] = new Falta(); // Crea el objeto en la celda actual
+                
+				// Llama a los métodos de la clase Falta para generar datos aleatorios
+               
+				this.alumnos[x][y].setInjustificada();
+                
+				this.alumnos[x][y].setJustificada();
+                
+				this.alumnos[x][y].setRetraso();
+           
+			}
+        }
+    }
+
+   
+	// Listado 1: Mayor número de faltas injustificadas por asignatura
+   
+	public void mayoresInjustificadas() {
+        
+		// Recorrido por COLUMNAS: Primero fijamos la asignatura (x)
+        
 		for (int x = 0; x < this.asignaturas.length; x++) {
-		    int mayor = -1;      
-		    int alumnoMax = -1;  
+            
+			int mayor = -1;      // Variable para guardar el record de faltas
+           
+            int alumnoMax = -1;  // Variable para guardar el índice del alumno ganador
 
-		   
-		    for (int y = 0; y < this.alumnos.length; y++) {
-		       
-		        int faltasActuales = this.alumnos[y][x].getInjustificada();
+            // Bajamos por todos los alumnos (y) para esa asignatura fija (x)
+          
+            for (int y = 0; y < this.alumnos.length; y++) {
+                
+            	int actuales = this.alumnos[y][x].getInjustificada();
+               
+            	if (actuales > mayor) { // Si encontramos a alguien con más faltas...
+                  
+            		mayor = actuales;   // Actualizamos el record
+                   
+            		alumnoMax = y;      // Guardamos quién es
+                }
+            }
+            
+            System.out.println("Asignatura: " + asignaturas[x] + " - Alumno con más injustificadas: " + alumnoMax + " (" + mayor + ")");
+        }
+    }
 
-		        if (faltasActuales > mayor) {
-		            mayor = faltasActuales;
-		            alumnoMax = y; 
-		        }
-		    }
-		    
-		   
-		    System.out.println("Asignatura: " + asignaturas[x] + 
-		                       " - Alumno con más faltas: " + alumnoMax + 
-		                       " (Faltas: " + mayor + ")");
-		}
-	
-	}
-	
-	public void retrasoSuperior(int i , String[] asignaturas) {
-			
-		int suma = 0;
-		
-		int contador = 0;
-		
-		for (int y = 0; y < i; y ++) {
-			
+    // Listado 2: Alumnos que superan la media global de retrasos
+   
+	public void alumnosSobreMediaRetrasos() {
+        
+		double sumaTotal = 0;
+        
+		int totalCeldas = alumnos.length * asignaturas.length;
+
+        // Paso A: Recorrido TOTAL para calcular la suma de todos los retrasos de la clase
+       
+		for (int y = 0; y < alumnos.length; y++) {
+           
 			for (int x = 0; x < asignaturas.length; x++) {
-				
-				
-				suma += alumnos[y][x].getRetraso();
-				
-				contador ++;
-				
-			}
+               
+				sumaTotal += alumnos[y][x].getRetraso();
+           
 			
-		}
-		
-		double media =(double) suma/contador;
-		
+			}
+        }
+       
+		double media = sumaTotal / totalCeldas; // Calculamos la media global
+        
 		System.out.println("Media global de retrasos: " + media);
-		
-		
+
+        // Paso B: Recorrido por FILAS para calcular el total de cada alumno y comparar
+       
 		for (int y = 0; y < this.alumnos.length; y++) {
-		    
-			double sumaAlumno = 0;
-		   
-		    for (int x = 0; x < this.asignaturas.length; x++) {
-		       
-		        sumaAlumno += this.alumnos[x][y].getRetraso();
+            
+			double sumaAlumno = 0; // Acumulador individual (se limpia para cada alumno)
+          
+			for (int x = 0; x < this.asignaturas.length; x++) {
+               
+				sumaAlumno += this.alumnos[y][x].getRetraso(); // Sumamos sus asignaturas
+            }
+            // Importante: El IF va fuera del bucle de asignaturas para comparar el total final
+            
+			if (sumaAlumno > media) {
+              
+				System.out.println("El alumno " + y + " supera la media con " + sumaAlumno + " retrasos totales.");
+          
+			}
+        }
+    }
 
-		        if (sumaAlumno > media) {
-		           
-		        	System.out.println("El alumno " + y + " supera la media con " + sumaAlumno + " retrasos.");
-		            
-		          
-		        }
-		        
-		    }
-		    
+    // Listado 3: Asignatura con el menor impacto total de retrasos
+    public void asignaturaMenorRetraso() {
+       
+    	// Inicializamos con el valor máximo posible para asegurar que cualquier suma sea menor
+       
+    	double menor = Double.MAX_VALUE; 
+       
+    	
+    	String asignaturaMenor = "";
 
-		}
-	}
-	
-	
-	public void retrasoMenor(int i , String[] asignaturas) {
-		
-		
-		
-	}
-	
+        // Recorrido por COLUMNAS: Sumamos los retrasos de todos los alumnos por cada asignatura
+        for (int x = 0; x < this.asignaturas.length; x++) {
+            
+        	double sumaAsignatura = 0; // Acumulador para la columna actual
+           
+        	for (int y = 0; y < this.alumnos.length; y++) {
+           
+            	sumaAsignatura += this.alumnos[y][x].getRetraso();
+            }
+
+            // Si el total de esta asignatura es el más bajo hasta ahora, lo guardamos
+           
+        	if (sumaAsignatura < menor) {
+                
+        		menor = sumaAsignatura;
+               
+        		asignaturaMenor = this.asignaturas[x];
+            }
+        }
+        
+        System.out.println("La asignatura con menos retrasos es: " + asignaturaMenor + " con un total de " + menor);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Programacion/Java/eclipse-workspace/tema5/Ejercicios POO y Arrays Avanzados /actividad3/Main.java
+++ Programacion/Java/eclipse-workspace/tema5/Ejercicios POO y Arrays Avanzados /actividad3/Main.java
@@ -1,20 +1,36 @@
 package actividad3;
-
 import java.util.*;
 
 public class Main {
+   
+    public static void main(String[] args) {
+    	
+    	Scanner teclado = new Scanner(System.in);
+    	
+        System.out.println("¿Cuántos alumnos tiene la clase?");
+       
+        int numAlumnos = teclado.nextInt();
+        
+        String[] nombresAsignaturas = {"Prog", "BD", "Sost", "HTML", "Siste"};
+        
+        // Creamos el objeto gestor
+        FaltasAlumnosAsignaturas gestion = new FaltasAlumnosAsignaturas(numAlumnos, nombresAsignaturas);
+        
+        //  Llenamos la matriz de objetos y datos
+        gestion.inicializarYReflejarDatos();
+        
+        // Ejecutamos los listados del enunciado
+        System.out.println("\n--- LISTADO MAYORES INJUSTIFICADAS ---");
+      
+        gestion.mayoresInjustificadas();
+        
+        System.out.println("\n--- ALUMNOS SOBRE LA MEDIA DE RETRASOS ---");
+       
+        gestion.alumnosSobreMediaRetrasos();
+        
+        System.out.println("\n--- ASIGNATURA CON MENOS RETRASOS ---");
+       
+        gestion.asignaturaMenorRetraso();
 
-	private static Scanner teclado = new Scanner(System.in);
-	
-	public static void main (String[]args) {
-		
-		int i = 0;
-		System.out.println("¿Cuantos alumnos tiene la clase?");
-		
-		 i = teclado.nextInt();
-		
-		String[] asignaturas = {"Prog", "BD", "Sost", "HTML", "Siste"};
-		
-		asignarMatriz(i, asignaturas);
-	}
+    }
 }
