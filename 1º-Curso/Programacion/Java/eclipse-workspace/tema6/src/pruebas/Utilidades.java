e35c35ca12963f90679f98f70ccae4c226f63674
RaulSalRom <salromeoraul@gmail.com>
2026-04-21 00:57:58 +0200

    java

---
+++ 1º-Curso/Programacion/Java/eclipse-workspace/tema6/src/pruebas/Utilidades.java
@@ -0,0 +1,87 @@
+package pruebas;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Arrays;
+
+public class Utilidades {
+	
+	public static void main(String[]args) {
+		//definimos los arrays
+		String[] origen  = {"Pepe","lola", "Sofia", "Andrea", "Hugo"};
+		String[] destino = new String[2];
+		Integer[] arrayOrigen = { 1, 2, 3, 4, 5};
+		Integer[] arrayDestino = new Integer[8];
+		
+		// llamada a los metodos
+		Utilidades.volcarArrays(origen, destino);
+		Utilidades.volcarArrays(arrayOrigen, arrayDestino);
+		
+		//imprimimos los arrays para ver los cambios
+		System.out.println(Arrays.toString(origen));
+		
+		System.out.println();
+		
+		System.out.println(Arrays.toString(destino));
+		
+		System.out.println();
+		
+		System.out.println(Arrays.toString(arrayOrigen));
+		
+		System.out.println();
+		
+		System.out.println(Arrays.toString(arrayDestino));
+		
+		
+		
+		
+		
+		
+		
+		//creamos un array dinamico en el que almacenamos strings
+		Collection<String> nombres = new ArrayList<>();
+		
+		//añadimos unos valores
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("Marta");
+        
+        //imprimimos el array
+        System.out.println(nombres);
+        
+        //contamos los contenidos del array
+        System.out.println("Número de elementos: " + nombres.size());
+        
+        //buscamos en el array
+        System.out.println("¿Está Luis? " + nombres.contains("Luis"));
+        
+        //Quitamos un elemento del array
+        nombres.remove("Ana");
+        
+        //imprimimos el resultado despues de haber borrado
+        System.out.println("Después de borrar Ana: " + nombres);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+		
+	}
+	//sin definir el type
+	public static <T> void volcarArrays(T[] origen, T[] destino) {
+		// math.min para elegir el más pequeño
+		int tamaño = Math.min(origen.length, destino.length);
+		//volcamos un array en otro
+		for(int i = 0; i < tamaño; i++) {
+			destino[i] = origen[i];
+		}
+		
+	}
+	
+}
