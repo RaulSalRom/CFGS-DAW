1da5a6a1eff22784f617a528f58b5824e52f088e
Raúl Sal Romeo <raulsalromeo@pc37-15.11000371.11.andared.ced.junta-andalucia.es>
2026-09-18 21:07:59 +0200

    entorno servidor

---
+++ 2º-Curso/Desarrollo web en entorno cliente/Prueba_inicial/prueba.js
@@ -0,0 +1,31 @@
+let a = "5";
+
+let b = 5;
+
+console.log(a == b);
+
+console.log(a === b);
+
+let number = [12, 3, 4, 5, 20, 80];
+
+console.log(number.filter(number => number > 10));
+
+var alumno ={"nombre": "Raul", "edad": 19, "asignatura": "Desarrollo web en entorno cliente", "notas": [10, 9, 8, 7, 6]};
+
+console.table(alumno);
+
+console.log(alumno);
+
+console.log(Object.keys(alumno));
+
+console.log(Object.values(alumno));
+
+console.log(alumno.nombre);     
+
+console.log(alumno.edad);
+
+console.log(alumno.asignatura);
+
+console.log(alumno.notas);
+
+console.log(alumno.notas[0]);
